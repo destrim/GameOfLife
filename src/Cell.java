f1288990368036abd866626cdcc2927d5382296d
slawek-p <slawek.potoczek@gmail.com>
2020-07-27 19:04:43 +0200

    Added lombok to the project.

--- src/Cell.java
+++ src/Cell.java
@@ -1,41 +1,13 @@
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Cell {
     private boolean isAlive;
     private Position pos;
-
-    public Cell(boolean isAlive, Position pos) {
-        this.isAlive = isAlive;
-        this.pos = pos;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
-    }
-
-    public void setAlive(boolean alive) {
-        isAlive = alive;
-    }
-
-    public Position getPos() {
-        return pos;
-    }
-
-    public void setPos(Position pos) {
-        this.pos = pos;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cell cell = (Cell) o;
-        return isAlive == cell.isAlive &&
-                pos.equals(cell.pos);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(isAlive, pos);
-    }
 }
